a3026a2891456d1e5e84e3d989351b1c05a5c5be
marcospre <ikbsj@plaiundi.net>
2023-01-08 20:13:14 +0100

    ni idea

---
+++ api/app/Http/Controllers/EmpresaController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Empresa;
+class EmpresaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $empresas = Empresa::all();
+        return $empresas;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $empresa = new Empresa();
+        $empresa->nombre = $request->nombre;
+        $empresa->datos = $request->datos;
+
+        $empresa->save(); 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $empresa = Empresa::findOrFail($request->$id);
+        $empresa->nombre = $request->nombre;
+        $empresa->datos = $request->datos;
+
+        $empresa->save();
+
+        return $empresa;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $empresa = Empresa::destroy($request->$id);
+        return $empresa;
+    }
+}
---
+++ api/routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/empresas', 'App\Http\Controllers\EmpresaController@index'); //mostrar todas la cotizaciones
+Route::post('/empresas', 'App\Http\Controllers\EmpresaController@store');//crear una empresa
+Route::put('/empresas/{id}', 'App\Http\Controllers\EmpresaController@update');//actualizar los datos de una empresa
+Route::delete('/empresas/{id}', 'App\Http\Controllers\EmpresaController@destroy');//actualizar los datos de una empresa
